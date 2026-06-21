c0da4a3fce962676f626c6b7cf5ac48235488646
dela-99 <ridgedela444@gmail.com>
2026-06-21 21:12:36 +0000

    vite change

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({
-      target: "react",
-      routesDirectory: "./src/routes",
-      generatedRouteTree: "./src/routeTree.gen.ts",
-    }),
-    react(),
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    viteReact(),
     tailwindcss(),
-    tsconfigPaths(),
+    tsConfigPaths(),
   ],
 });
